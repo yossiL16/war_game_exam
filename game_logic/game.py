e0ba52ef54b586303658fc4f1fad957817a9b7d7
yossi lerer <ylerer770@gmail.com>
2025-10-23 09:48:04 +0300

    add fils of project

---
+++ game_logic/game.py
@@ -0,0 +1,16 @@
+
+
+
+def create_player(name:str) -> dict:
+    pass
+
+
+
+
+def init_game()->dict:
+    pass
+
+
+def play_round(p1:dict,p2:dict):
+    pass
+
---
+++ utils/deck.py
@@ -0,0 +1,13 @@
+
+
+
+def create_card(rank:str,suite:str) -> dict:
+    pass
+def compare_cards(p1_card:dict, p2_card:dict) -> str:
+    pass
+
+def create_deck() -> list[dict]:
+    pass
+
+def shuffle(deck:list[dict]) -> list[dict]:
+    pass
